cfa6cfa0bbb2946ec44cebd0cadb7d4b8f43ad62
Daniel Sanches <danielsanches6301@gmail.com>
2022-03-26 11:17:44 -0300

    Criando Interfaces e services na camada de aplicaçao

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -1,11 +1,7 @@
 ﻿using CleanArchMvc.Domain.Entities;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Application.DTOs
 {
