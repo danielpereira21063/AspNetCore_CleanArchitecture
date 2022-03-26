c9c25f14d2271a710980d8efc42e658c9c541dd8
Daniel Sanches <danielsanches6301@gmail.com>
2022-03-26 10:50:44 -0300

    Criando DTOs e mapeamentos

---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,39 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; private set; }
+        [Required(ErrorMessage = "The description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public string Description { get; private set; }
+        [Required(ErrorMessage = "The price is required")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; private set; }
+        [Required(ErrorMessage = "The stock is required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; private set; }
+        [MaxLength(250)]
+        [DisplayName("Product image")]
+        public string Image { get; private set; }
+        public int CategoryId { get; set; }
+        [DisplayName("Categories")]
+        public Category Category { get; set; }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Migrations/20220326002221_SeedProducts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = string.Empty;
+            sql += "INSERT INTO Products\n";
+            sql += "(Name, Description, Price, Stock, Image, CategoryId) VALUES\n";
+            sql += "('Caderno espiral', '100 folhas', 7.45,50, 'caderno1.jpg', 1),\n";
+            sql += "('Estojo escolar', 'Estojo escolar cinza', 5.65,70, 'estojo1.jpg', 1),\n";
+            sql += "('Borracha escolar', 'Borracha branca pequena', 3.25,80, 'borracha1.jpg', 1),\n";
+            sql += "('Calculadora escolar', 'Calculadora simples', 15.39,20, 'calculadora1.jpg', 2);\n";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
